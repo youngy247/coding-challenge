ef0235945bbd7ed926c121804996d90e77f0c15f
Adam Young <adamyoungy678@gmail.com>
2023-10-27 13:45:00 +0100

    feat: Add helper functions for term processing and matching

--- findTerms.js
+++ findTerms.js
@@ -1,66 +1,52 @@
 function findTermInstances(text, terms) {
-  // Define common pronoun groupings for easier term matching.
   const pronouns = {
     firstPersonSingular: ["I", "me", "my", "mine", "myself"],
     firstPersonPlural: ["we", "us", "our", "ours", "ourselves"],
     secondPersonSingular: ["you", "your", "yourself"],
   };
 
-  // Initialize an empty array to store terms that match from the text.
-  const matchedTerms = [];
+  const isPronoun = (term) => {
+    for (let key in pronouns) {
+      if (pronouns[key].map((p) => p.toLowerCase()).includes(term)) {
+        return pronouns[key];
+      }
+    }
+    return false;
+  };
 
-  // Split the input terms by comma, trim them and convert to lowercase.
+  const cleanWord = (word) => {
+    return word.replace(/[^a-zA-Z]/g, "");
+  };
+
+  const addToMatchedTerms = (word, termsToCheck, matchedTerms) => {
+    if (
+      termsToCheck.includes(word) ||
+      termsToCheck.includes(word.toLowerCase())
+    ) {
+      if (!matchedTerms.includes(word)) {
+        matchedTerms.push(word);
+      }
+    }
+  };
+
   const termList = terms.split(",").map((term) => term.trim().toLowerCase());
-
-  // Split the input text by spaces and remove any non-alphabetical characters from each word.
-  const words = text.split(" ").map((word) => word.replace(/[^a-zA-Z]/g, ""));
-
-  // Initialize an array to store the final list of terms we need to check against the text.
+  const words = text.split(" ").map(cleanWord);
+  const matchedTerms = [];
   let termsToCheck = [];
 
-  // Loop over each term in the termList.
-  for (let i = 0; i < termList.length; i++) {
-    let isPronoun = false;
-
-    // Extract the keys (types of pronoun groupings) from the pronouns object.
-    const pronounTypes = Object.keys(pronouns);
-
-    // Loop over each pronoun type to check if the term is a pronoun.
-    for (let j = 0; j < pronounTypes.length; j++) {
-      // If the current term is found in any of the pronoun lists,
-      // add all pronouns from that list to termsToCheck.
-      if (
-        pronouns[pronounTypes[j]]
-          .map((p) => p.toLowerCase())
-          .includes(termList[i])
-      ) {
-        termsToCheck = termsToCheck.concat(pronouns[pronounTypes[j]]);
-        isPronoun = true;
-        break; // exit the loop once a match is found.
-      }
+  termList.forEach((term) => {
+    const pronounList = isPronoun(term);
+    if (pronounList) {
+      termsToCheck = termsToCheck.concat(pronounList);
+    } else {
+      termsToCheck.push(term);
     }
+  });
 
-    // If the current term wasn't a pronoun, add it directly to termsToCheck.
-    if (!isPronoun) {
-      termsToCheck.push(termList[i]);
-    }
-  }
+  words.forEach((word) => {
+    addToMatchedTerms(word, termsToCheck, matchedTerms);
+  });
 
-  // Loop over each word in the text.
-  for (let i = 0; i < words.length; i++) {
-    // If the word (or its lowercase version) is in termsToCheck and not yet in matchedTerms,
-    // add it to matchedTerms.
-    if (
-      termsToCheck.includes(words[i]) ||
-      termsToCheck.includes(words[i].toLowerCase())
-    ) {
-      if (!matchedTerms.includes(words[i])) {
-        matchedTerms.push(words[i]);
-      }
-    }
-  }
-
-  // Return the list of matched terms.
   return matchedTerms;
 }
 
